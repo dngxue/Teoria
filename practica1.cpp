2279ac2b73f77b989b37aae89075bf9b1fe2a5a1
dngxue <122043669+dngxue@users.noreply.github.com>
2023-10-15 19:46:36 -0600

    le puse colores

--- practica1.cpp
+++ practica1.cpp
@@ -47,15 +47,15 @@ void leerAlfabeto(string &alfa, string &repAlfa)
 	alfa = "";
 	repAlfa = "";
 	
-	cout<<"\t1) DEFINIR UN ALFABETO"<<endl;
+	cout<<"\t\033[96m1) DEFINIR UN ALFABETO\033[0m"<<endl;
 	cout<<"\t1. El alfabeto debe tener al menos tres simbolos"<<endl;
 	cout<<"\t2. Los simbolos pueden ser ingresados:"<<endl;
-	cout<<"\t\t a. De manera individual\t Ejemplo: a, b, c, e"<<endl;
-	cout<<"\t\t b. Por rango\t Ejemplo: m-x"<<endl;
+	cout<<"\t\t a. De manera individual\t \033[33mEjemplo: a, b, c, e\033[0m"<<endl;
+	cout<<"\t\t b. Por rango\t \033[33mEjemplo: m-x\033[0m"<<endl;
 	
 	do{
 
-		cout<<"\tIngrese la opcion en \"a\" o \"b\""<<endl;
+		cout<<"\tIngrese la opcion en \033[31ma\033[0m o \033[31mb\033[0m"<<endl;
 		cin >> op;
 
 		if(op == 'a'){
@@ -139,12 +139,12 @@ void leerAlfabeto(string &alfa, string &repAlfa)
 				}
 				else
 				{
-					cout<<"Ingrese el alfabeto con el formato correcto: "<<endl;
+					cout<<"Ingrese el alfabeto con el formato correcto:"<<endl;
 				}
 			}
 		}
 		else{
-			cout<<"\n\tOpcion invalida"<<endl;
+			cout<<"\n\033[31mOpcion invalida\033[0m"<<endl;
 		}
 
 	}while(op != 'a' && op != 'b');	
@@ -154,37 +154,37 @@ void leerCadenas(string &w1, string &w2, string alfa)
 {
 	// Cero o más ocurrencias del elemento anterior
 	regex alfabeto(alfa + "*");
-	cout<<"\n\t2) INGRESAR DOS CADENAS SOBRE EL ALFABETO DEFINIDO"<<endl;
+	cout<<"\n\t\033[96m2) INGRESAR DOS CADENAS SOBRE EL ALFABETO DEFINIDO\033[0m"<<endl;
 	cout<<"\t1. Ingrese dos cadenas w1 y w2"<<endl;
 	cout<<"\t2. Ambos deben ser elementos del alfabeto definido"<<endl;
 
 	do{
 		w1 = "";
-		cout<<"\na. Ingrese la cadena w1:"<<endl;
+		cout<<"\n\033[95ma. Ingrese la cadena w1:\033[0m"<<endl;
 		cin>>w1;
 
 		if(regex_match(w1,alfabeto))
 		{
-			cout<<"Candena valida\n";
+			cout<<"\033[32mCadena valida\033[0m\n";
 			break;
 		}else
 		{
-			cout<<"Cadena invalida\n";
+			cout<<"\033[31mCadena invalida\033[0m\n";
 		}
 	}while(true);
 
 	do{
 		w2 = "";
-		cout<<"\nb. Ingrese la cadena w2:"<<endl;
+		cout<<"\n\033[95mb. Ingrese la cadena w2:\033[0m"<<endl;
 		cin>>w2;
 
 		if(regex_match(w2,alfabeto))
 		{
-			cout<<"Candena valida\n";
+			cout<<"\033[32mCadena valida\033[0m\n";
 			break;
 		}else
 		{
-			cout<<"Cadena invalida\n";
+			cout<<"\033[31mCadena invalida\033[0m\n";
 		}
 	}while(true);
 
@@ -196,7 +196,7 @@ void compararCadenas(string &w1, string &w2)
 {
 	string cadena = "";
 
-	cout<<"\n\n\t3) COMPARAR AMBAS CADENAS"<<endl;
+	cout<<"\n\t\033[96m3) COMPARAR AMBAS CADENAS\033[0m"<<endl;
 	cout<<"\tComparacion entre la cadena w1 y w2\n\n";
 	cout<<"\tw1 respecto a w2:"<<endl;
 	
@@ -283,23 +283,23 @@ void expresionRegular()
 	system("cls");
 	regex expresionr("^[a-df-hj-np-tv-z]*a[a-df-hj-np-tv-z]*e[b-hj-np-tv-z]*i[b-df-np-tv-z]*o[b-df-hj-tv-z]*u[b-df-hj-np-z]*$");
 
-	cout<<"\n\n\t7) IMPLEMENTACION DE UNA EXPRESION REGULAR"<<endl;
-	cout<<"\t1. Todas las cadenas de letras en minusculas (a-z) que contengan las 5 vocales en orden."<<endl;
-	cout<<"\t2. Las vocales pueden estar repetidas siempre que mantengan el orden."<<endl;
-	cout<<"\t3. La secuencia completa de vocales tambien puede repetirse."<<endl;
-	cout<<"\t\ta. Ejemplo de cadena valida: rtaeioujutf"<<endl;
-	cout<<"\t\tb. Ejemplo de cadena invalida: kayteemnoyug"<<endl;
+	cout<<"\t\033[96m7) IMPLEMENTACION DE UNA EXPRESION REGULAR\033[0m"<<endl;
+	cout<<"\t1. Todas las cadenas de letras en minusculas (a-z) que contengan las 5 vocales en orden"<<endl;
+	cout<<"\t2. Las vocales pueden estar repetidas siempre que mantengan el orden"<<endl;
+	cout<<"\t3. La secuencia completa de vocales tambien puede repetirse"<<endl;
+	cout<<"\t\ta. \033[33mEjemplo de cadena valida: rtaeioujutf\033[0m"<<endl;
+	cout<<"\t\tb. \033[33mEjemplo de cadena invalida: kayteemnoyug\033[0m"<<endl;
 
 	while(true)
 	{
 		cin>>a;
 		if(regex_match(a,expresionr))
 		{
-			cout<<"Cadena valida"<<endl;
+			cout<<"\033[32mCadena valida\033[0m\n";
 			break;
 		}else
 		{
-			cout<<"Cadena invalida"<<endl;
+			cout<<"\033[31mCadena invalida\033[0m\n";
 		}
 	}
 
