a59df9aaa4abaf9e655f2afb7a8e6bcfb7a31187
dngxue <122043669+dngxue@users.noreply.github.com>
2023-10-17 19:26:43 -0600

    Add files via upload

--- practica1.cpp
+++ practica1.cpp
@@ -1,9 +1,11 @@
 /*
+Práctica No. 1 Alfabetos, lenguajes y expresiones regulares
+
+Autores:	Olvera Martínez Aarón
+			Reyes Francisco Paola
 
 Para compilar:
 gcc practica1.cpp -o p1.exe -lstdc++
-
-
 
 */
 
@@ -22,7 +24,7 @@ void compararCadenas(string &w1, string &w2);
 bool palabraRepetida(vector<string> &L, string s);
 void crearLenguajes(vector<char> &letrasAlfa);
 void diferenciaLenguajes(vector<string> &L1, vector<string> &L2);
-void alfabetoPotencia(vector<char> &letrasAlfa, int potencia);
+void alfabetoPotencia(vector<char> &letrasAlfa);
 void expresionRegular();
 
 int main()
@@ -34,14 +36,18 @@ int main()
     //cadenas
     string w1;
     string w2;
-
+    int potencia;
+    
     leerAlfabeto(alfa, repAlfa, letrasAlfa);
     regex alfabeto(alfa);
+
     cout<<"\nEl alfabeto ingresado es: "<<repAlfa<<endl;
-    //leerCadenas(w1,w2,alfa);
-	//crearLenguajes(letrasAlfa);
-    //expresionRegular();
-	alfabetoPotencia(letrasAlfa, 0);
+
+    leerCadenas(w1,w2,alfa);
+	crearLenguajes(letrasAlfa);
+	alfabetoPotencia(letrasAlfa);
+
+	expresionRegular();
 
 	return 0;
 }
@@ -58,8 +64,8 @@ void leerAlfabeto(string &alfa, string &repAlfa, vector<char> &letrasAlfa)
 	cout<<"\t\033[96m1) DEFINIR UN ALFABETO\033[0m"<<endl;
 	cout<<"\t1. El alfabeto debe tener al menos tres simbolos"<<endl;
 	cout<<"\t2. Los simbolos pueden ser ingresados:"<<endl;
-	cout<<"\t\t a. De manera individual\t \033[33mEjemplo: a, b, c, e\033[0m"<<endl;
-	cout<<"\t\t b. Por rango\t \033[33mEjemplo: m-x\033[0m"<<endl;
+	cout<<"\t\t a. De manera individual \t \033[33mEjemplo: a,b,c,e (separado por comas y sin espacios)\033[0m"<<endl;
+	cout<<"\t\t b. Por rango\t\t\t \033[33mEjemplo: m-x\033[0m"<<endl;
 	
 	do{
 
@@ -68,7 +74,7 @@ void leerAlfabeto(string &alfa, string &repAlfa, vector<char> &letrasAlfa)
 
 		if(op == 'a'){
 
-			cout<<"\nIngrese el alfabeto:"<<endl;
+			cout<<"\n\033[95m Ingrese el alfabeto:\033[0m"<<endl;
 
 			bool tresSimbolos = false;
 
@@ -107,7 +113,7 @@ void leerAlfabeto(string &alfa, string &repAlfa, vector<char> &letrasAlfa)
 				if(cont >= 3)	tresSimbolos = true;
 				else
 				{
-					cout << "\nIngrese de nuevo el alfabeto con al menos 3 símbolos:" << endl;
+					cout << "\n\033[95m Ingrese de nuevo el alfabeto con al menos 3 simbolos:\033[0m" << endl;
 				}	
 			}
 			for(char c: alfa){
@@ -124,7 +130,7 @@ void leerAlfabeto(string &alfa, string &repAlfa, vector<char> &letrasAlfa)
 		}
 		else if(op == 'b')
 		{
-			cout << "\nIngrese el alfabeto por rango:" << endl;
+			cout << "\n\033[95m Ingrese el alfabeto por rango:\033[0m" << endl;
 			bool guion = false;
 
 			while(!guion)
@@ -148,16 +154,16 @@ void leerAlfabeto(string &alfa, string &repAlfa, vector<char> &letrasAlfa)
 				}
 				else if (alfa.length() == 3 && alfa[1] == '-' && alfa[2] - alfa[0] < 2 )
 				{
-					cout<<"\nIngrese de nuevo el alfabeto con al menos 3 símbolos y en orden correcto:"<<endl;
+					cout<<"\n\033[95m Ingrese de nuevo el alfabeto con al menos 3 simbolos y en orden correcto:\033[0m"<<endl;
 				}
 				else
 				{
-					cout<<"Ingrese el alfabeto con el formato correcto:"<<endl;
+					cout<<"\033[95m\n Ingrese el alfabeto con el formato correcto:\033[0m"<<endl;
 				}
 			}
 		}
 		else{
-			cout<<"\n\033[31mOpcion invalida\033[0m"<<endl;
+			cout<<"\n\033[31m Opcion invalida\033[0m"<<endl;
 		}
 
 	}while(op != 'a' && op != 'b');	
@@ -173,31 +179,31 @@ void leerCadenas(string &w1, string &w2, string alfa)
 
 	do{
 		w1 = "";
-		cout<<"\n\033[95ma. Ingrese la cadena w1:\033[0m"<<endl;
+		cout<<"\n\033[95ma. Ingrese la cadena w1:\033[0m ";
 		cin>>w1;
 
 		if(regex_match(w1,alfabeto))
 		{
-			cout<<"\033[32mCadena valida\033[0m\n";
+			cout<<"\033[92mCadena valida\033[0m\n";
 			break;
 		}else
 		{
-			cout<<"\033[31mCadena invalida\033[0m\n";
+			cout<<"\033[31mCadena invalida\033[0m, ingrese otra opcion\n";
 		}
 	}while(true);
 
 	do{
 		w2 = "";
-		cout<<"\n\033[95mb. Ingrese la cadena w2:\033[0m"<<endl;
+		cout<<"\n\033[95mb. Ingrese la cadena w2:\033[0m ";
 		cin>>w2;
 
 		if(regex_match(w2,alfabeto))
 		{
-			cout<<"\033[32mCadena valida\033[0m\n";
+			cout<<"\033[92mCadena valida\033[0m\n";
 			break;
 		}else
 		{
-			cout<<"\033[31mCadena invalida\033[0m\n";
+			cout<<"\033[31mCadena invalida\033[0m, ingrese otra opcion\n";
 		}
 	}while(true);
 
@@ -211,7 +217,7 @@ void compararCadenas(string &w1, string &w2)
 
 	cout<<"\n\t\033[96m3) COMPARAR AMBAS CADENAS\033[0m"<<endl;
 	cout<<"\tComparacion entre la cadena w1 y w2\n\n";
-	cout<<"\tw1 respecto a w2:"<<endl;
+	cout<<"\t\033[93mw1\033[0m respecto a \033[93mw2\033[0m:"<<endl;
 	
 	/* PREFIJO (PROPIO O NO PROPIO) */
 
@@ -229,7 +235,7 @@ void compararCadenas(string &w1, string &w2)
 		}
 	}else
 	{
-		cout<<"\tNO es PREFIJO"<<endl;	
+		cout<<"\t\033[31mNO\033[0m es PREFIJO"<<endl;	
 	}
 
 	/* SUFIJO (PROPIO O NO PROPIO) */
@@ -248,7 +254,7 @@ void compararCadenas(string &w1, string &w2)
 		}
 	}else
 	{
-		cout<<"\tNO es SUFIJO"<<endl;	
+		cout<<"\t\033[31mNO\033[0m es SUFIJO"<<endl;	
 	}
 
 	/* SUBCADENA (PROPIO O NO PROPIO) */
@@ -261,11 +267,11 @@ void compararCadenas(string &w1, string &w2)
 			cout<<"\tes SUBCADENA PROPIA"<<endl;
 		}else
 		{
-			cout<<"\tes SUBCADENA NO PROPIA"<<endl;
+			cout<<"\tes SUBCADENA \033[31mNO\033[0m PROPIA"<<endl;
 		}
 	}else
 	{
-		cout<<"\tNO es SUBCADENA"<<endl;	
+		cout<<"\t\033[31mNO\033[0m es SUBCADENA"<<endl;	
 	}
 
 	/* SUBSECUENCIA */
@@ -282,41 +288,13 @@ void compararCadenas(string &w1, string &w2)
 		cout<<"\tes SUBSECUENCIA"<<endl;
 	}else
 	{
-		cout<<"\tNO es SUBSECUENCIA"<<endl;
+		cout<<"\t\033[31mNO\033[0m es SUBSECUENCIA"<<endl;
 	}
 
+	printf("\n\n");
 	system("pause");
 }
 
-void expresionRegular()
-{
-	int op;
-	string a = "";
-
-	system("cls");
-	regex expresionr("^[a-df-hj-np-tv-z]*a[a-df-hj-np-tv-z]*e[b-hj-np-tv-z]*i[b-df-np-tv-z]*o[b-df-hj-tv-z]*u[b-df-hj-np-z]*$");
-
-	cout<<"\t\033[96m7) IMPLEMENTACION DE UNA EXPRESION REGULAR\033[0m"<<endl;
-	cout<<"\t1. Todas las cadenas de letras en minusculas (a-z) que contengan las 5 vocales en orden"<<endl;
-	cout<<"\t2. Las vocales pueden estar repetidas siempre que mantengan el orden"<<endl;
-	cout<<"\t3. La secuencia completa de vocales tambien puede repetirse"<<endl;
-	cout<<"\t\ta. \033[33mEjemplo de cadena valida: rtaeioujutf\033[0m"<<endl;
-	cout<<"\t\tb. \033[33mEjemplo de cadena invalida: kayteemnoyug\033[0m"<<endl;
-
-	while(true)
-	{
-		cin>>a;
-		if(regex_match(a,expresionr))
-		{
-			cout<<"\033[32mCadena valida\033[0m\n";
-			break;
-		}else
-		{
-			cout<<"\033[31mCadena invalida\033[0m\n";
-		}
-	}
-
-}
 
 bool palabraRepetida(vector<string> &L, string s){
 	for(string u: L){
@@ -330,7 +308,6 @@ void imprimirLenguaje(vector<string> &L){
 		cout << L[i] << ", ";
 	}
 	cout << L[L.size()-1] << "}\n";
-
 }
 
 void crearLenguajes(vector<char> &letrasAlfa){
@@ -345,12 +322,16 @@ void crearLenguajes(vector<char> &letrasAlfa){
 	int max = letrasAlfa.size() - 1;
 	uniform_int_distribution<int> dist(min, max);
 
-	cout<<"\t\033[96m4) GENERAR LOS LENGUAJES L1 Y L2\033[0m"<<endl;
-	cout<<"\t1. Para el lenguaje L1, ingrese el numero de palabras seguido de su longitud"<<endl;
+	cout<<"\n\t\033[96m4) GENERAR LOS LENGUAJES L1 Y L2\033[0m"<<endl;
+	cout<<"\t1. Con base en el alfabeto definido, generar los lenguajes L1 y L2 de forma aleatoria"<<endl;
+	cout<<"\t2. Debera ingresar el numero de palabras (\033[31mnp\033[0m) asi como su longitud (\033[31ml\033[0m)"<<endl;
+	cout<<"\t\033[33mEjemplo: 4 6\033[0m"<<endl;
+
+	cout<<"\n\033[95m a. Para el lenguaje L1, ingrese el numero de palabras seguido de su longitud:\033[0m"<<endl;
 	
 	bool opInvalida = false;
 	do{
-		if(opInvalida == true) cout << "\tOpcion invalida" << endl;
+		if(opInvalida == true) cout << "\n\033[31mOpcion invalida\033[0m" << endl;
 		cin >> np >> l;
 		opInvalida = true;
 	}while(np > pow(letrasAlfa.size(), l));
@@ -367,9 +348,9 @@ void crearLenguajes(vector<char> &letrasAlfa){
 		}
 	}
 
-	cout<<"\t1. Para el lenguaje L2, ingrese el numero de palabras seguido de su longitud"<<endl;
-
+	cout<<"\n\033[95m b. Para el lenguaje L2, ingrese el numero de palabras seguido de su longitud:\033[0m"<<endl;
 	cin >> np >> l;
+	printf("\n");
 
 	i = 0;
 	while(i < np){
@@ -383,41 +364,51 @@ void crearLenguajes(vector<char> &letrasAlfa){
 		}
 	}
 
-	cout << "L1 = {";
+	cout << "\033[93m L1\033[0m = {";
 	imprimirLenguaje(L1);
-	cout << "L2 = {";
+	cout << "\033[93m L2\033[0m = {";
 	imprimirLenguaje(L2);
-
+	printf("\n\n");
 	diferenciaLenguajes(L1, L2);
 
 }
 
-void diferenciaLenguajes(vector<string> &L1, vector<string> &L2){
-	cout<<"\t\033[96m5) GENERAR LD = L1 - L2\033[0m"<<endl;
+void diferenciaLenguajes(vector<string> &L1, vector<string> &L2)
+{
 	vector<string> LD;
+
+	cout<<"\t\033[96m5) GENERAR LD = L1 - L2\033[0m"<<endl;
+	cout<<"\t1. Generar el lenguaje LD resultado de la diferencia de los lenguajes"<<endl;
+	cout<<""<<endl;
+
 	for(string s: L1){
 		if(!palabraRepetida(L2, s))
 			LD.push_back(s);
 	}
 	if(!LD.empty()){
-		cout << "LD = {";
+		cout << " \033[93mLD\033[0m = {";
 		imprimirLenguaje(LD);
 	}
 	else{
-		cout << "LD = Lenguaje vacio \n";
+		cout << " \033[93mLD\033[0m = Lenguaje vacio \n";
 	}
 }
 
-void alfabetoPotencia(vector<char> &letrasAlfa, int potencia){
+void alfabetoPotencia(vector<char> &letrasAlfa){
 	queue<string> q;
 	string aux;
-	int i, j;
+	int i, j, potencia;
 	vector<string> alfaPotencia;
 	q.push("");
 	int np_parcial;
 
+	cout<<"\n\t\033[96m6) OBTENER LA POTENCIA DEL ALFABETO DEFINIDO\033[0m"<<endl;
+	cout<<"\t1. El valor de la potencia es un numero entero (rango: -5 a 5)"<<endl;
+	cout<<"\t2. Ingrese la potencia\t \033[33mEjemplo: -3\033[0m"<<endl;
+	cin>>potencia;
+
 	if(potencia == 0){
-		cout << "AlfaPotencia = {lambda}\n";
+		cout << "\n \033[91mAlfaPotencia\033[0m = {lambda}\n";
 		return;
 	}
 
@@ -445,5 +436,36 @@ void alfabetoPotencia(vector<char> &letrasAlfa, int potencia){
 
 	cout << "AlfaPotencia = {";
 	imprimirLenguaje(alfaPotencia);
+	printf("\n\n");
+	system("pause");
+
+}
+
+void expresionRegular()
+{
+	int op;
+	string a = "";
+
+	regex expresionr("^[a-df-hj-np-tv-z]*a[a-df-hj-np-tv-z]*e[b-hj-np-tv-z]*i[b-df-np-tv-z]*o[b-df-hj-tv-z]*u[b-df-hj-np-z]*$");
+
+	cout<<"\n\t\033[96m7) IMPLEMENTACION DE UNA EXPRESION REGULAR\033[0m"<<endl;
+	cout<<"\t1. Todas las cadenas de letras en minusculas (a-z) que contengan las 5 vocales en orden"<<endl;
+	cout<<"\t2. Las vocales pueden estar repetidas siempre que mantengan el orden"<<endl;
+	cout<<"\t3. La secuencia completa de vocales tambien puede repetirse"<<endl;
+	cout<<"\t\ta. \033[33mEjemplo de cadena valida: rtaeioujutf\033[0m"<<endl;
+	cout<<"\t\tb. \033[33mEjemplo de cadena invalida: kayteemnoyug\033[0m"<<endl;
+
+	while(true)
+	{
+		cin>>a;
+		if(regex_match(a,expresionr))
+		{
+			cout<<"\033[32mCadena valida\033[0m\n";
+			break;
+		}else
+		{
+			cout<<"\033[31mCadena invalida, vuelva a ingresar la cadena\033[0m\n";
+		}
+	}
 
 }
